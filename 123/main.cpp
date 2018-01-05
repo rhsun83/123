fefcb13d029be092190c6a964d73b744c7c5a1a2
Peter Sun <rhsun83@gmail.com>
2018-01-05 19:50:07 +0800

    Initial Commit

---
+++ 123/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  123
+//
+//  Created by Ruey-horng Sun on 2018/1/5.
+//  Copyright © 2018年 Ruey-horng Sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
